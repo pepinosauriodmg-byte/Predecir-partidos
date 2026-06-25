a82700f81b355e2835c4dca9c7cc8807a6566a2f
pepinosauriodmg-byte <pepinosaurio.dmg@gmail.com>
2026-06-25 00:44:41 -0600

    Update app.py

--- app.py
+++ app.py
@@ -398,12 +398,12 @@ with col_principal:
         st.markdown("<p style='color: #aedef7; font-size: 0.9rem; margin-bottom: 20px;'>Análisis avanzado y contexto histórico de los contendientes.</p>", unsafe_allow_html=True)
         
         partidos_manana = [
-            ('Switzerland', 'Canada'), 
-            ('Bosnia and Herzegovina', 'Qatar'),     
-            ('Morocco', 'Haiti'),
-            ('Scotland', 'Brazil'), 
-            ('South Africa', 'South Korea'),     
-            ('Czechia', 'Mexico')
+            ('Curaçao', 'Ivory Coast'), 
+            ('Ecuador', 'Germany'),     
+            ('Tunisia', 'Netherlands'),
+            ('Japan', 'Sweden'), 
+            ('Türkiye', 'USA'),     
+            ('Paraguay', 'Australia')
         ]
         
         # ELIMINAMOS LAS FUNCIONES DUPLICADAS AQUÍ ADENTRO. AHORA USA LAS DE ARRIBA.
